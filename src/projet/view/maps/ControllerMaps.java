adccf30554fef4e40077454aa2e1b5c65a7b95c9
JulienMorelli <julienantibes98@gmail.com>
2020-05-31 01:37:28 +0200

    Ajout GoogleMaps

---
+++ src/projet/view/maps/ControllerMaps.java
@@ -0,0 +1,37 @@
+package projet.view.maps;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.util.converter.IntegerStringConverter;
+import projet.data.Equipe;
+
+public class ControllerMaps {
+	
+	@FXML
+    private WebView webView;
+	@FXML
+	private Button addMarker;
+	
+	@FXML
+	private void initialize() {
+
+		// Data binding
+		WebEngine webEngine = webView.getEngine();
+        webEngine.load( getClass().getResource("maps.html").toString() );
+		
+        String param = "document.addMarker(\"test\",45.818971, 1.272345)";
+        addMarker.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                webEngine.executeScript(param);
+            }
+        });
+		
+	}
+	 
+}
