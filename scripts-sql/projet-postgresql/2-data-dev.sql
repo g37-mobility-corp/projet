8c0dbb7b2054f121e8e3becb20a4ca2906573667
JulienMorelli <julienantibes98@gmail.com>
2020-06-11 19:34:09 +0200

    modif maps + modif data bdd

--- scripts-sql/projet-postgresql/2-data-dev.sql
+++ scripts-sql/projet-postgresql/2-data-dev.sql
@@ -66,9 +66,18 @@ UPDATE equipe SET idchef = 3, idcoequipier = 4 WHERE idequipe =  1;
 -- Poste
 
 INSERT INTO poste (idposte, nom, lieu, fonction ) VALUES
-  ( 1, 'Poste n°1', 'Buvette', 'Descriptif 1' ),
-  ( 2, 'Poste n°2', 'Parking', 'Descriptif 2' ),
-  ( 3, 'Poste n°3', 'Arrivée', 'Descriptif 3' );
+  ( 1, 'Parking Voiture', 'Parking', 'Descriptif 1' ),
+  ( 2, 'Parking Velo', 'Parking', 'Descriptif 2' ),
+  ( 4, 'Remise des dossards', 'Départ', 'Descriptif 3' );
+  ( 5, 'Signaleur', 'Parcours', 'Descriptif 3' );
+  ( 6, 'Ravitaillement', 'Parcours', 'Descriptif 3' );
+  ( 7, 'Sécurité sur eau', 'Parcours', 'Descriptif 3' );
+  ( 8, 'Chronométrage', 'Départ/Arrivée', 'Descriptif 3' );
+  ( 9, 'Moto', 'Fermeture parcours', 'Descriptif 3' );
+  ( 10, 'Buvette', 'Buvette', 'Descriptif 3' );
+  ( 11, 'Repas', 'Repas', 'Descriptif 3' );
+  ( 12, 'Récuperer les dossards et puces', 'Départ', 'Descriptif 3' );
+  ( 13, 'Photographe', 'Mobile', 'Descriptif 3' );
 
 ALTER TABLE poste ALTER COLUMN idposte RESTART WITH 4;
 
